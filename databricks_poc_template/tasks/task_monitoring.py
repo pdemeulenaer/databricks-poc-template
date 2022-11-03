eda55602897bd25c9b023ca8f8fb32be113cf9d2
Philippe de Meulenaer <31767740+pdemeulenaer@users.noreply.github.com>
2022-11-03 17:50:55 +0200

    load right version of data in monitoring

--- databricks_poc_template/tasks/task_monitoring.py
+++ databricks_poc_template/tasks/task_monitoring.py
@@ -18,10 +18,10 @@ from evidently.model_profile.sections import DataDriftProfileSection, Classifica
 from evidently.pipeline.column_mapping import ColumnMapping
 
 
-class InferenceTask(Task):
+class MonitoringTask(Task):
 
     # Custom function
-    def _inference(self, **kwargs):
+    def _monitoring(self, **kwargs):
         # ===========================
         # 0. Reading the config files
         # ===========================
@@ -35,8 +35,9 @@ class InferenceTask(Task):
         self.logger.info("input configs: {0}".format(input_conf))
 
         db_in = input_conf["database"]  
-        scored_inference_dataset = input_conf["scored_inference_dataset"] 
-        test_dataset = input_conf["test_dataset"]            
+        train_dataset = input_conf["train_dataset"]  
+        test_dataset = input_conf["test_dataset"]  
+        scored_inference_dataset = input_conf["scored_inference_dataset"]                  
 
         # Output
         output_conf = self.conf["data"]["output"]
@@ -64,93 +65,58 @@ class InferenceTask(Task):
 
         try:
 
-            # Load the inference raw data
-            raw_data = spark.table(f"{db_in}.{inference_dataset}") # Should we load all of it, every time???
+            # Extract the right version of the training dataset (as logged in MLflow)
+            client = mlflow.tracking.MlflowClient()
+            run = client.get_run(latest_model.run_id)
+            train_dataset_version = run.data.tags['train_dataset_version']
+            test_dataset_version = run.data.tags['test_dataset_version']
 
-            # max_date = raw_data.select("date").rdd.max()[0]
-            # print(max_date)
-            # raw_data = raw_data.withColumn("filter_out", when(col("date")==max_date,"1").otherwise(0)) # don't take last day of data
-            # raw_data = raw_data.filter("filter_out==1").drop("filter_out")
-            # display(raw_data)
+            train_dataset = spark.read.option("versionAsOf", train_dataset_version).table(f"{db_in}.{train_dataset}")
+            test_dataset = spark.read.option("versionAsOf", test_dataset_version).table(f"{db_in}.{test_dataset}")
+            scored_inference_dataset = spark.table(f"{db_in}.{scored_inference_dataset}")                        
 
-            self.logger.info("Step 1. completed: Loading the Inference data")   
+            self.logger.info("Step 1. completed: Loading the data")   
           
         except Exception as e:
-            print("Errored on 1.: Loading the Inference data")
+            print("Errored on 1.: Loading the data")
             print("Exception Trace: {0}".format(e))
             # print(traceback.format_exc())
             raise e    
 
         # ========================================
-        # 2. Model inference
-        # ========================================
-        try:   
-            # Initialize the Feature Store client
-            fs = feature_store.FeatureStoreClient()
+        # 2. Data monitoring
+        # ========================================  
 
-            # Get the model URI
-            latest_model = module.get_latest_model_version(model_name,"databricks")
-            latest_model_version = int(latest_model.version)
-            model_uri = f"models:/" + model_name + f"/{latest_model_version}"
+        try:       
 
-            # Call score_batch to get the predictions from the model
-            df_with_predictions = fs.score_batch(model_uri, raw_data)
-            display(df_with_predictions)    
+            train_dataset_pd = train_dataset.toPandas()
+            train_dataset_pd.drop('target', inplace=True, axis=1)
+            
+            # Data drift calculation
+            data_columns = ColumnMapping()
+            data_columns.numerical_features = train_dataset_pd.columns #['sl_norm', 'sw_norm', 'pl_norm', 'pw_norm']
+            data_drift_profile = Profile(sections=[DataDriftProfileSection()])
+            df_with_predictions_pd = df_with_predictions.toPandas()
+            print(train_dataset_pd.columns)
+            print(df_with_predictions_pd.columns)
+            data_drift_profile.calculate(train_dataset_pd, df_with_predictions_pd, column_mapping=data_columns) 
+            data_drift_profile_dict = json.loads(data_drift_profile.json())
+            print(data_drift_profile.json())
+            print(data_drift_profile_dict['data_drift'])
+            
+            # Save the data monitoring to data lake 
+            data_monitor_json = json.dumps(data_drift_profile_dict['data_drift'])
+            data_monitor_df = spark.read.json(sc.parallelize([data_monitor_json]))
+            display(data_monitor_df)
+            data_monitor_df.write.option("header", "true").format("delta").mode("overwrite").save(cwd+"data_monitoring")
 
-            # Write scored data
-            df_with_predictions.write.format("delta").mode("overwrite").option("overwriteSchema", "true").saveAsTable(f"{db_out}.{scored_inference_dataset}")                             
-
-            # print("Step 2. completed: model inference")  
-            self.logger.info("Step 2. completed: model inference")                
+            self.logger.info("Step 2 completed: data monitoring")  
 
         except Exception as e:
-            print("Errored on step 2.: model inference")
+            print("Errored on step 2: data monitoring")
             print("Exception Trace: {0}".format(e))
             print(traceback.format_exc())
-            raise e
-
-        # # ========================================
-        # # 3. Data monitoring
-        # # ========================================  
-
-        # try:       
-        #     # Extract the right version of the training dataset (as logged in MLflow)
-        #     client = mlflow.tracking.MlflowClient()
-        #     run = client.get_run(latest_model.run_id)
-        #     train_dataset_version = run.data.tags['train_dataset_version']
-        #     train_dataset_path = run.data.tags['train_dataset_path']
-        #     # test_dataset_version = run.data.tags['test_dataset_version']
-        #     # fs_table_version = run.data.tags['fs_table_version']
-        #     train_dataset = spark.read.format("delta").option("versionAsOf", train_dataset_version).load(train_dataset_path)
-        #     train_dataset_pd = train_dataset.toPandas()
-
-        #     train_dataset_pd.drop('target', inplace=True, axis=1)
-            
-        #     # Data drift calculation
-        #     data_columns = ColumnMapping()
-        #     data_columns.numerical_features = train_dataset_pd.columns #['sl_norm', 'sw_norm', 'pl_norm', 'pw_norm']
-        #     data_drift_profile = Profile(sections=[DataDriftProfileSection()])
-        #     df_with_predictions_pd = df_with_predictions.toPandas()
-        #     print(train_dataset_pd.columns)
-        #     print(df_with_predictions_pd.columns)
-        #     data_drift_profile.calculate(train_dataset_pd, df_with_predictions_pd, column_mapping=data_columns) 
-        #     data_drift_profile_dict = json.loads(data_drift_profile.json())
-        #     print(data_drift_profile.json())
-        #     print(data_drift_profile_dict['data_drift'])
-            
-        #     # Save the data monitoring to data lake 
-        #     data_monitor_json = json.dumps(data_drift_profile_dict['data_drift'])
-        #     data_monitor_df = spark.read.json(sc.parallelize([data_monitor_json]))
-        #     display(data_monitor_df)
-        #     data_monitor_df.write.option("header", "true").format("delta").mode("overwrite").save(cwd+"data_monitoring")
-
-        #     self.logger.info("Step 1.2 completed: data monitoring")  
-
-        # except Exception as e:
-        #     print("Errored on step 1.2: data monitoring")
-        #     print("Exception Trace: {0}".format(e))
-        #     print(traceback.format_exc())
-        #     raise e        
+            raise e        
 
 
         # # try:
@@ -201,16 +167,19 @@ class InferenceTask(Task):
         # #     print("Errored on step 1.2: data monitoring")
         # #     print("Exception Trace: {0}".format(e))
         # #     print(traceback.format_exc())
-        # #     raise e           
+        # #     raise e    
+
+
+       
 
     def launch(self):
-        self.logger.info("Launching inference task")
-        self._inference()
-        self.logger.info("Inference task finished!")  
+        self.logger.info("Launching monitoring task")
+        self._monitoring()
+        self.logger.info("Monitoring task finished!")  
 
 # if you're using python_wheel_task, you'll need the entrypoint function to be used in setup.py
 def entrypoint():  # pragma: no cover
-    task = InferenceTask()
+    task = MonitoringTask()
     task.launch()
 
 # if you're using spark_python_task, you'll need the __main__ block to start the code execution
