a7227849d0cd5015c9c48e8a283e2fe2904010f7
Philippe de Meulenaer <31767740+pdemeulenaer@users.noreply.github.com>
2022-11-03 18:19:13 +0200

    add performance monitoring

--- databricks_poc_template/tasks/task_monitoring.py
+++ databricks_poc_template/tasks/task_monitoring.py
@@ -37,13 +37,16 @@ class MonitoringTask(Task):
         db_in = input_conf["database"]  
         train_dataset = input_conf["train_dataset"]  
         test_dataset = input_conf["test_dataset"]  
-        scored_inference_dataset = input_conf["scored_inference_dataset"]                  
+        scored_inference_dataset = input_conf["scored_inference_dataset"]  
+        label_table = input_conf["label_table"]                
 
         # Output
         output_conf = self.conf["data"]["output"]
         self.logger.info("output configs: {0}".format(output_conf))       
 
         db_out = output_conf["database"] 
+        data_monitoring = output_conf["data_monitoring"] 
+        performance_monitoringing = output_conf["performance_monitoringing"] 
 
         # Model configs
         model_conf = self.conf["model"]
@@ -76,7 +79,8 @@ class MonitoringTask(Task):
 
             train_dataset = spark.read.option("versionAsOf", train_dataset_version).table(f"{db_in}.{train_dataset}")
             test_dataset = spark.read.option("versionAsOf", test_dataset_version).table(f"{db_in}.{test_dataset}")
-            df_with_predictions = spark.table(f"{db_in}.{scored_inference_dataset}")                        
+            df_with_predictions = spark.table(f"{db_in}.{scored_inference_dataset}")   
+            label_table =  spark.table(f"{db_in}.{label_table}")                       
 
             self.logger.info("Step 1. completed: Loading the data")   
           
@@ -120,58 +124,47 @@ class MonitoringTask(Task):
             print(traceback.format_exc())
             raise e        
 
+        # ========================================
+        # 3. Performance monitoring  (Here assumption of no delayed outcome!)
+        # ========================================   
+        try:        
 
-        # # try:
-        # # ========================================
-        # # 1.3 Performance monitoring  (Here assumption of no delayed outcome!)
-        # # ========================================            
+            test_dataset_pd = test_dataset.toPandas()
 
-        # # Extract the right version of the training dataset (as logged in MLflow)
-        # client = mlflow.tracking.MlflowClient(tracking_uri=tracking_uri, registry_uri=registry_uri)
-        # run = client.get_run(latest_model.run_id)
-        # train_dataset_version = run.data.tags['train_dataset_version']
-        # train_dataset_path = run.data.tags['train_dataset_path']
-        # # test_dataset_version = run.data.tags['test_dataset_version']
-        # # fs_table_version = run.data.tags['fs_table_version']
-        # train_dataset = spark.read.format("delta").option("versionAsOf", train_dataset_version).load(train_dataset_path)
-        # train_dataset_pd = train_dataset.toPandas()
+            # Load the target labels of the unseen data (the ones we tried to infer in step 1.1). Here is the assumption of no delayed outcome... 
+            df_with_predictions = df_with_predictions.join(label_table, ['Id','hour'])
+            
+            # Performance drift calculation
+            data_columns = ColumnMapping()
+            data_columns.target = 'target'
+            data_columns.prediction = 'prediction'
+            list_columns = [item for item in test_dataset_pd.columns if item not in ['target','prediction']]
+            data_columns.numerical_features = list_columns #['sl_norm', 'sw_norm', 'pl_norm', 'pw_norm']
 
-        # # Load the target labels of the unseen data (the ones we tried to infer in step 1.1). Here is the assumption of no delayed outcome... 
-        # labels = spark.read.format('delta').load(cwd + 'labels')
-        # df_with_predictions = df_with_predictions.join(labels, ['Id','hour'])
-        
-        # # Performance drift calculation
-        # data_columns = ColumnMapping()
-        # data_columns.target = 'target'
-        # data_columns.prediction = 'prediction'
-        # data_columns.numerical_features = train_dataset_pd.columns #['sl_norm', 'sw_norm', 'pl_norm', 'pw_norm']
+            performance_drift_profile = Profile(sections=[ClassificationPerformanceProfileSection()])
+            df_with_predictions_pd = df_with_predictions.toPandas()
+            print(test_dataset_pd.columns)
+            print(df_with_predictions_pd.columns)
+            print(test_dataset_pd.head())
+            print(df_with_predictions_pd.head())
+            performance_drift_profile.calculate(test_dataset_pd, df_with_predictions_pd, column_mapping=data_columns) 
+            performance_drift_profile_dict = json.loads(performance_drift_profile.json())
+            print(performance_drift_profile.json())
+            print(performance_drift_profile_dict)
+            
+            # Save the data monitoring to data lake 
+            performance_monitoring_json = json.dumps(performance_drift_profile_dict)
+            performance_monitoring_df = spark.read.json(sc.parallelize([performance_monitoring_json]))
+            print(performance_monitoring_df)
+            performance_monitoring_df.write.format("delta").mode("overwrite").option("overwriteSchema", "true").saveAsTable(f"{db_out}.{performance_monitoring}") 
 
-        # performance_drift_profile = Profile(sections=[ClassificationPerformanceProfileSection()])
-        # df_with_predictions_pd = df_with_predictions.toPandas()
-        # print(train_dataset_pd.columns)
-        # print(df_with_predictions_pd.columns)
-        # print(train_dataset_pd.head())
-        # print(df_with_predictions_pd.head())
-        # performance_drift_profile.calculate(train_dataset_pd, df_with_predictions_pd, column_mapping=data_columns) 
-        # performance_drift_profile_dict = json.loads(performance_drift_profile.json())
-        # print(performance_drift_profile.json())
-        # print(performance_drift_profile_dict)
-        
-        # # Save the data monitoring to data lake 
-        # performance_monitor_json = json.dumps(performance_drift_profile_dict)
-        # performance_monitor_df = spark.read.json(sc.parallelize([performance_monitor_json]))
-        # print(performance_monitor_df)
-        # performance_monitor_df.write.option("header", "true").format("delta").mode("overwrite").save(cwd+"performance_monitoring")
+            self.logger.info("Step 1.3 completed: performance monitoring")  
 
-        # self.logger.info("Step 1.3 completed: performance monitoring")  
-
-        # # except Exception as e:
-        # #     print("Errored on step 1.2: data monitoring")
-        # #     print("Exception Trace: {0}".format(e))
-        # #     print(traceback.format_exc())
-        # #     raise e    
-
-
+        except Exception as e:
+            print("Errored on step 1.2: data monitoring")
+            print("Exception Trace: {0}".format(e))
+            print(traceback.format_exc())
+            raise e    
        
 
     def launch(self):
